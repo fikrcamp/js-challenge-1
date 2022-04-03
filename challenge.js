015a6a80c67a0ddda5a88fced537a8dd11174eff
Yuhanaz <yoonis7881@gmail.com>
2022-04-03 08:58:11 +0300

    BMI exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let aliHeight = 1.71;
+let aliWeight = 79.4;
+let bmi = aliWeight / (aliHeight**2);
+
+
+let mohamedHeight = 1.82;
+let mohamedWeight = 85.6;
+let bmi1 = mohamedWeight / (mohamedHeight**2);
+
+
+console.log(bmi < bmi1)
+
