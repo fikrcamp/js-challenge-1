a6f13b7dbf871c1032ace076292742755f032efa
muk7tar <mukhtar.osmna7177@gmail.com>
2022-11-12 12:53:06 +0300

    exercise 3

--- challenge.js
+++ challenge.js
@@ -1,2 +1,34 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+// ## Javascript Fundementals Challenge One (Part One)💪🏾
+//  data 1
+
+mahmedmass = 78
+mahmedhight = 1.69
+alimass = 92
+alihight =1.95
+
+mahmedBMI = mahmedmass / mahmedhight ** 2 
+console.log(mahmedBMI)
+
+aliBMI = alimass/ alihight ** 2
+console.log(aliBMI)
+
+MohamedHigherBMI = mahmedBMI > aliBMI
+console.log(MohamedHigherBMI)
+
+// data 2
+
+mahmedmass = 95
+mahmedhight = 1.88
+alimass = 85
+alihight =1.79
+
+mahmedBMI = mahmedmass / mahmedhight ** 2 
+console.log(mahmedBMI)
+
+aliBMI = alimass/ alihight ** 2
+console.log(aliBMI)
+
+MohamedHigherBMI = mahmedBMI > aliBMI
+console.log(MohamedHigherBMI)
