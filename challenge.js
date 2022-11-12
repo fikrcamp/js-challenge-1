1488bac5b6d4c754853adc85688f8ba1e6093284
mohan omar <mohaomar338@gmail.com>
2022-11-12 20:20:34 +0300

    waa iga haysaa

--- challenge.js
+++ challenge.js
@@ -1,2 +1,38 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+
+
+let mass = 78
+let heigh = 1.69
+let masss = 92
+let height = 1.95
+
+
+    console.log(mass)
+    console.log(heigh)
+    console.log(masss)
+    console.log(height)
+
+    console.log(mass / (heigh * heigh))
+    console.log(masss / (height * height))
+
+   let MohamedHigherBMI = true
+
+    console.log("mohamed has a higher BMI than Ali")
+   
+let mweight = 95
+let mheigh = 1.88
+let Aliweight = 85
+let aliheigh = 1.79
+
+
+    console.log(mweight)
+    console.log(mheigh)
+    console.log(Aliweight)
+    console.log(aliheigh)
+
+    console.log(mweight / (mheigh * mheigh))
+    console.log(Aliweight/ (aliheigh * aliheigh))
+
+let MohamedHigherBMI2 = true
+    console.log("mohamed has a higher BMI than Ali")
+
+
