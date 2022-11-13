ce3ba6a9390a9539bdb458536c275cd00fbb88a9
unknown <maxamedkaka999@gmail.com>
2022-11-13 19:34:49 +0300

    done js challenge 1

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let mohamedmass= 78
+let mohamedheight = 1.69
+
+console.log(mohamedmass / (mohamedheight*mohamedheight))
+
+let Alimass = 92 
+let ALiheightand =1.95
+
+console.log(Alimass/ (ALiheightand*ALiheightand))
+
+let MohamedHigherBMI = true
+console.log(MohamedHigherBMI)
