02ecd839155252d5ccfdb72ee39a6a036e28ae42
Abdirahman Mohamed (Buushe) <79105703+Buushe@users.noreply.github.com>
2022-11-14 10:27:04 +0600

    done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let mass1 = 80 //60
+let heigth1 = 1.86
+let mass2 = 70 //90
+let height2 = 1.85
+ BMI1 = mass1/(heigth1*heigth1)
+ BMI2 = mass2/(height2*height2)
+
+if(mass1 > mass2){
+    console.log("Mohamed is higher BMI than Ali")
+}else{
+    console.log("Ali is higher BMI than Mohamed")
+}
