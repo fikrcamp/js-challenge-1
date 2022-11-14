2f6a6d15f9a3c9ca7de3672fb31da377718a18d4
Abdilahi mohamed <cabdale3377@gmail.com>
2022-11-14 10:45:17 +0300

    calculated the BMI --JS-CHALLENGE-1

--- challenge.js
+++ challenge.js
@@ -1,2 +1,19 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+// mohamed data
+
+let mMass = 70;
+let mHeight = 1.6;
+
+let BMI = mMass / mHeight ** 2;
+//  ali data
+
+let aMass = 87;
+let aHeight = 1.5;;
+
+let aBMI = aMass / aHeight ** 2;
+
+
+if(BMI > aBMI){
+  console.log(`mohamed weight is ${mMass} kg and ${mHeight} m tall, higher than ali ${aMass} kg and ${aHeight} m`);
+}else
+console.log(`Ali weight is ${aMass} kg and ${aHeight} m tall, higher than mohamed ${mMass} kg and ${mHeight} m`)
+
