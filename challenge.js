69b02158e016ef3ee263f2c84fc73d19d96d14ba
hamda <hamda.baashe44@gmail.com>
2022-11-14 08:20:26 +0000

    finished Js-challenge-1

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let Ali ={
+    weight:92,
+    height:1.95,
+    aliBMI:function(){
+       return this.weight/this.height**2;
+       
+    }
+}
+ 
+
+let Mohamed ={
+    weight:78,
+    height:1.69,
+    MoBMI:function(){
+       return this.weight/this.height**2;
+       
+    }
+}
+let MohamedHigherBMI = (Mohamed.MoBMI() > Ali.aliBMI());
+console.log(MohamedHigherBMI);
