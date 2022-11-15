08eaec45717d339f7285cc885c058c2876638fc7
mohammoudomar <emperror180@gmail.com>
2022-11-15 18:47:16 +0300

    js-challenge-1 done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let mohamedmass=78
+let mohamedheight=1.69
+
+let alimass=92
+let aliheight=1.95
+
+
+let BMImohamed=mohamedmass / mohamedheight ** 2
+let BMIali=alimass / aliheight ** 2
+
+let mohamedhigherBMI=true
+
+if (BMImohamed > BMIali){
+    console.log(`${mohamedhigherBMI} mohamed has a higher BMI then ali`)
+}
+else{
+    console.log("ali has a higher BMI then mohamed")
+}
+
