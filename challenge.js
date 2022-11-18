3a95ad4cdf4a59d12b1300a5f22ebe34cf34a7d9
unknown <hudeyfiabdirahman6@GMAIL.COM>
2022-11-18 17:01:04 +0300

    bmi exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let hudeyfimass=60
+let hudeyfiheight=1.79
+
+let hamsemass=50
+let hamseheight=1.85
+
+
+let BMIhudeyfi = hudeyfimass / hudeyfiheight ** 2
+let BMIhamse = hamsemass / hamseheight ** 2
+
+let hudeyfihigherBMI=true
+
+if (BMIhudeyfi > BMIhamse){
+    console.log(`${hudeyfihigherBMI} hudeyfi has a higher BMI then hamse`)
+}
+else{
+    console.log("hamse has a higher BMI then hudeyfi")
+}
+
+
